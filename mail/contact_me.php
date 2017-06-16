fb99c3aa32e1a0f2e6c498e00212d92e4ad81e5f
Andrew <andrewsmiththefirst@gmail.com>
2017-06-16 13:44:39 -0400

    email

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -8,17 +8,17 @@ if(empty($_POST['name'])  		||
 	echo "No arguments Provided!";
 	return false;
    }
-	
+
 $name = $_POST['name'];
 $email_address = $_POST['email'];
 $message = $_POST['message'];
-	
-// create email body and send it	
-$to = 'YOUR-EMAIL@YOUR-WEBSITE.com'; // ****PUT YOUR EMAIL ADDRESS HERE****
+
+// create email body and send it
+$to = 'friendsupped@gmail.com'; // ****PUT YOUR EMAIL ADDRESS HERE****
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: CONTACT-FORM@YOUR-WEBSITE.com\n"; // ****ENTER WHO YOU WANT THE MESSAGE TO BE FROM HERE****
-$headers .= "Reply-To: $email_address";	
+$headers = "From: friendsupped@gmail.com\n"; // ****ENTER WHO YOU WANT THE MESSAGE TO BE FROM HERE****
+$headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
-return true;			
+return true;
 ?>
